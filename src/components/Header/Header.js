acfd00e327daabd57929ce0cd129a70f48c29a93
Jessica Paul <paul.jessica95@gmail.com>
2021-05-19 16:05:54 -0400

    add basic components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Clay Inventory App</h1>
+            <ul>
+                <li>Brand</li>
+                <li>Color</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default Header;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,9 @@
+const Item = () => {
+    return (
+        <div className="Item">
+            # Item Name Qty
+        </div>
+    );
+}
+ 
+export default Item;
