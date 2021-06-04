4a93089ed59ea0f47d40853d06bfeebd71d27d36
Jessica Paul <paul.jessica95@gmail.com>
2021-06-04 16:40:08 -0400

    add sort menu to inventory

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -3,7 +3,15 @@ import './Inventory.css';
 const Inventory = () => {
     return (
         <div className="Inventory">
-            Inventory Page!
+            <h2>My Inventory</h2>
+            <div className="sorting">
+                <label htmlFor="sort">Sort By:</label>
+                <select name="sort" id="sort">
+                    <option value="brand">Brand</option>
+                    <option value="color">Color</option>
+                    <option value="quantity">Quantity</option>
+                </select>
+            </div>
         </div>
     );
 }
