27127e333eebe1999d55b6c12d54894b4ae08aaa
Jessica Paul <paul.jessica95@gmail.com>
2021-06-04 16:28:41 -0400

    add colors page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Brands from './pages/Brands/Brands';
 import Inventory from './pages/Inventory/Inventory';
+import Colors from './pages/Colors/Colors';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -30,6 +31,9 @@ function App() {
         } />
         <Route exact path = '/brands' render={(props) => 
           <Brands />
+        } />
+        <Route exact path = '/colors' render={(props) => 
+          <Colors />
         } />
         <Route exact path = '/inventory' render={(props) => 
           <Inventory />
---
+++ src/pages/Colors/Colors.js
@@ -0,0 +1,11 @@
+import './Colors.css';
+
+const Colors = () => {
+    return (
+        <div className="Colors">
+            Colors Page!
+        </div>
+    );
+}
+ 
+export default Colors;
